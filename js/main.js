7be9bb1c0a4e79af1a6976f99e3c8367c37e08ab
Abuzarhaider <abuzarsarwary3@gmail.com>
2023-03-27 02:35:20 +0500

    JS functionality added

--- js/main.js
+++ js/main.js
@@ -19,9 +19,15 @@ window.addEventListener("load", () => {
         displayNote();
 
     })
+
     displayNote();
 })
 
+
+
+/**********
+    DISPLAY
+************/
 let displayNote = () => {
     const noteSection = document.querySelector("#section");
     noteSection.innerHTML = "";
@@ -29,145 +35,83 @@ let displayNote = () => {
     noteData.forEach(note => {
         const article = document.createElement("article");
         article.innerHTML = `<div class="tools">
-                                <abbr title="Edit"><img src="./icons/edit.svg" id="editBtn" alt="Edit note"></abbr>
-                                <abbr title="Delete"><img src="./icons/trash.svg" id="deleteBtn"  alt="Delete note"></abbr>
+                                <abbr title="Edit"><img src="./icons/edit.svg" id="editBtn" onclick="editNote('${note.title}', '${note.description}')" alt="Edit note"></abbr>
+                                <abbr title="Delete"><img src="./icons/trash.svg" id="deleteBtn" onclick="deleteNote('${note.title}')"  alt="Delete note"></abbr>
                             </div>
                             <h3 class="noteTitle">${note.title}</h3>
                             <p class="noteDescription">${note.description}</p>`;
         section.appendChild(article);
-
-
-
-        let deleteBtn = document.querySelector("#deleteAll");
-        deleteBtn.addEventListener('click', () => {
-            let deleteAll_text = "Do you want to delete all notes permanetly?";
-            if (confirm(deleteAll_text) == true) {
-                noteData = noteData.filter(e => e != note);
-                localStorage.setItem("noteData", JSON.stringify(noteData));
-                displayNote();
-            }
-
-        })
-
-
-
-        // let deleteBtn = () => {
-        //     console.log("clicked");
-        //     noteData = noteData.filter(t => t != note);
-        //     localStorage.setItem("noteData", JSON.stringify(noteData));
-        //     displayNote();
-        // }
     });
 }
 
 
 
-// Delete
+/**************
+    DELETE ALL
+***************/
+let deleteAll = document.querySelector("#deleteAll");
+deleteAll.addEventListener('click', () => {
+
+    if ("noteData" in localStorage === false) {
+        alert("No Note Found")
+    }
+    else {
+        let deleteAlert = confirm("All notes will be deleted");
+        if (deleteAlert) {
+            localStorage.clear();
+            window.location.reload();
+        }
+    }
+})
+
+
+
+/**********
+    EDIT
+**********/
+let editNote = (titleEdit, descEdit) => {
+
+    asideNoteDiv.style.left = "0";
+    document.getElementById("title").value = titleEdit;
+    document.getElementById("description").value = descEdit;
+
+    let indexing = noteData.findIndex(x => x.title === titleEdit);
+
+    if (indexing > -1) {
+        noteData.splice(indexing, 1);
+    }
+
+    localStorage.setItem("noteData", JSON.stringify(noteData));
+    displayNote();
+
+
+}
+
+
+
+/*****************
+    DELETE SINGLE
+******************/
+let deleteNote = (titleEdit) => {
+
+    if (confirm("This note will be deleted.")) {
+        let indexing = noteData.findIndex(x => x.title === titleEdit);
+
+        if (indexing > -1) {
+            noteData.splice(indexing, 1);
+            displayNote();
+        }
+
+        localStorage.setItem("noteData", JSON.stringify(noteData));
+    }
+}
 
 
 
 
-
-// let editBtn = null;
-
-
-// // Create
-// let submitData = () => {
-//     let data = retrieveData();
-//     let localData = storeData(data);
-//     console.log(data);
-
-//     // document.querySelector("#title").value = "";
-//     // document.querySelector("#description").value = "";
-
-//     insertData(localData);
-//     return false;
-
-//     // if(editBtn = null){
-//     //     insertData(localData);
-//     //     return false;
-//     // }
-//     // else{
-//     //     update();
-//     //     return false;
-//     // }
-// }
-
-// // Create ------> Take data
-// let retrieveData = () => {
-//     title = document.querySelector("#title").value;
-//     decData = document.querySelector("#description").value;
-//     arrData = JSON.parse(localStorage.getItem("arrData")) || [];
-
-//     let objData = {
-//         title: title,
-//         description: decData
-//     }
-
-//     arrData.push(objData);
-//     localStorage.setItem("arrData", arrData);
-
-//     return arrData;
-// }
-
-// // Create ------> Store data in local
-// let storeData = (data) => {
-//     // let localTitle = localStorage.setItem("localTitle", data[0]);
-//     // let localDescription = localStorage.setItem("localDescription", data[1]);
-
-//     let titleGet = data[0].title;
-//     let descriptionGet = data[1].description;
-
-//     let arrGet = [titleGet, descriptionGet];
-
-//     return arrGet;
-// }
-
-// // Create ------> INSERT
-// let insertData = (localData) => {
-//     let section = document.getElementById("section");
-//     let article = document.createElement("article");
-
-//     article.innerHTML = `<div class="tools">
-//                             <abbr title="Edit"><img src="./icons/edit.svg" onclick="editNote(this)" alt="Edit note"></abbr>
-//                             <abbr title="Delete"><img src="./icons/trash.svg" onclick="deleteNote(this)" alt="Delete note"></abbr>
-//                         </div>
-//                         <h3 class="noteTitle">${localData[0]}</h3>
-//                         <p class="noteDescription">${localData[1]}</p>`;
-//     section.appendChild(article);
-// }
-
-
-// // Edit
-// let editNote = (x)=>{
-//     editBtn = x.parentElement.parentElement.nextElementSibling;
-
-//     document.getElementById("title").value = editBtn.innerHTML;
-//     document.getElementById("description").value = editBtn.nextElementSibling.innerHTML;
-
-//     asideNoteDiv.style.left = "0";
-// }
-
-
-// // Update
-// let update = ()=>{
-//     editBtn.innerHTML = document.getElementById("title").value;
-//     editBtn.nextElementSibling.innerHTML = document.getElementById("description").value;
-//     editBtn = null;
-// }
-
-// // Delete
-// let deleteNote = () => {
-//     confirm("Do you want to delete this note?");
-// }
-
-// // localStorage.clear();
-
-
-
-
-
-// aside
+/*********
+    Aside
+**********/
 let asideNoteDiv = document.querySelector(".asideNoteDiv");
 let newNote = () => {
     asideNoteDiv.style.left = "0";
@@ -179,5 +123,9 @@ let closeNewNote = () => {
 }
 
 let asideIn = () => {
-    asideNoteDiv.style.left = "-100%";
+    let titleInput = document.querySelector("#title");
+    let descriptionInput = document.querySelector("#description")
+
+    if (titleInput.value != "" && descriptionInput.value != "")
+        asideNoteDiv.style.left = "-100%";
 }
